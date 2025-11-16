cdd8b1e092501c02fd2fd4842ac5fd8c2bfec2ae
Nabid-Ahmad <nabidahmad.zidan@gmail.com>
2025-11-16 16:59:37 +0600

    Update ALLOWED_HOSTS to allow all hosts

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ay9d8fz9jbb9_sdy1e(*g-*c=t!qs-&1lw8o!czg*k+tc2(f#w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
